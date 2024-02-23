0a2e95497f31a540d4c2f08304b8abb9d2e787de
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-23 22:17:09 +0530

    added task form

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
+import vutify from './vutify'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+createApp(App).use(vutify).mount('#app')
---
+++ src/vutify.js
@@ -0,0 +1,8 @@
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify'
+
+export default createVuetify({
+  components,
+  directives
+})
