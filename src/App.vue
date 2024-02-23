4a820e30cc65e8a62d30b832e9cd653f19b68e12
sameer <sameertrip123@example.com>
2024-02-23 23:51:48 +0530

    Added Register Form Component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <LoginForm />
-  <TaskForm />
+  <!-- <LoginForm /> -->
+  <RegisterForm />
+  <!-- <TaskForm /> -->
 </template>
-
-
 
 <script>
 import LoginForm from './components/auth/LoginForm.vue'
+import RegisterForm from './components/auth/RegisterForm.vue'
 import TaskForm from './components/tasks/TaskForm.vue';
 
 export default {
   components: {
     LoginForm,
-    TaskForm
+    RegisterForm,
+    TaskForm,
   }
 }
 </script>
