f6a8e2a05cd81cadc6b1e74df1aab017eee2be7a
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-24 19:34:59 +0530

    Moved greeting to a new component

--- src/components/dashboard/DashBoard.vue
+++ src/components/dashboard/DashBoard.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="px-10 py-6 flex xl:flex-row flex-col">
     <div class="left flex-1">
-      <div
-        class="bg-slate-200 py-10 px-5 rounded-lg flex flex-col items-center text-center text-slate-900"
-      >
-        <h1 class="text-5xl font-bold">Hello Sagnik!</h1>
-        <p class="mt-4 text-xl">It's good to see you again.</p>
-      </div>
+      <greeting-vue></greeting-vue>
       <task-list class="mt-8"></task-list>
     </div>
 
@@ -14,6 +9,7 @@
   </div>
 </template>
 <script setup>
+import GreetingVue from './Greeting.vue'
 import TaskList from './TaskList/TaskList.vue'
 </script>
 <style scoped>
---
+++ src/components/dashboard/Greeting.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="bg-slate-200 py-10 px-5 rounded-lg flex flex-col items-center text-center text-slate-900"
+  >
+    <h1 class="text-5xl font-bold">Hello Sagnik!</h1>
+    <p class="mt-4 text-xl">It's good to see you again.</p>
+  </div>
+</template>
+<script setup></script>
