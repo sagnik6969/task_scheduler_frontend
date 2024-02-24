c142d30272b9de2efbefc8ae93494ed6c30eebf0
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-24 10:18:58 +0530

    added submit tast to backend feature

--- src/components/ui/IconSelect.vue
+++ src/components/ui/IconSelect.vue
@@ -5,6 +5,9 @@
     <v-icon :icon="icon"></v-icon>
     <select
       class="p-0 font-medium border-non appearance-none border-none focus:[box-shadow:none] bg-transparent"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :value="modelValue"
+      :required="required"
     >
       <slot></slot>
     </select>
@@ -12,7 +15,20 @@
 </template>
 
 <script setup>
-defineProps(['icon'])
+defineProps({
+  icon: {
+    type: String
+  },
+  modelValue: {
+    type: String,
+    default: 'Choose on Option'
+  },
+  required: {
+    default: false,
+    type: Boolean
+  }
+})
+defineEmits(['update:modelValue'])
 </script>
 <style scoped>
 select {
