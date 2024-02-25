176ad97b8c8926bc00c5e6398dd52615b391fa39
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-26 01:35:12 +0530

    added task stats to user dashboard

--- src/components/dashboard/right_column/TaskCounter.vue
+++ src/components/dashboard/right_column/TaskCounter.vue
@@ -3,7 +3,7 @@
     <div
       class="flex-1 bg-slate-200 rounded-lg py-6 px-6 flex-col md:flex-row flex justify-center items-center space-x-3"
     >
-      <h1 class="text-6xl font-extrabold text-slate-900">11</h1>
+      <h1 class="text-6xl font-extrabold text-slate-900">{{ completedTasks }}</h1>
       <div class="font-semibold text-slate-700">
         <p>Tasks</p>
         <p>completed</p>
@@ -13,7 +13,7 @@
     <div
       class="flex-1 bg-slate-200 rounded-lg py-6 px-6 flex-col md:flex-row flex justify-center items-center space-x-3"
     >
-      <h1 class="text-6xl font-extrabold text-slate-900">4</h1>
+      <h1 class="text-6xl font-extrabold text-slate-900">{{ pendingTasks }}</h1>
       <div class="font-semibold text-slate-700">
         <p>Tasks</p>
         <p>in Progress</p>
@@ -21,3 +21,25 @@
     </div>
   </div>
 </template>
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+const completedTasks = ref(0)
+const pendingTasks = ref(0)
+
+onMounted(() => {
+  axios
+    .get(`/api/user/analysis`, {
+      params: {
+        time_range: 'all',
+        statistics: 'completed_vs_pending_tasks'
+      }
+    })
+    .then((res) => {
+      // console.log(res.data)
+      completedTasks.value = res.data.series[0]
+      pendingTasks.value = res.data.series[1]
+    })
+})
+</script>
--- src/components/dashboard/right_column/statistics/Statistics.vue
+++ src/components/dashboard/right_column/statistics/Statistics.vue
@@ -1,47 +1,77 @@
 <template>
-  <div>
+  <div class="hidden sm:block">
     <h1 class="text-2xl font-semibold text-slate-900">Your Statistics</h1>
     <div class="mt-3 space-x-3">
       <select
         class="border-none focus:[box-shadow:none] bg-slate-200 rounded-md shadow font-medium text-slate-900"
+        v-model="statistics"
       >
-        <option value="" selected>Completed vs Pending Tasks</option>
-        <option value="">Task Distribution by Progress</option>
-        <option value="">Task Distribution by Priority</option>
-        <option value="">Task Distribution by Deadline</option>
+        <option value="completed_vs_pending_tasks" selected>Completed vs Pending Tasks</option>
+        <option value="task_distribution_by_progress">Task Distribution by Progress</option>
+        <option value="task_distribution_by_priority">Task Distribution by Priority</option>
+        <!-- <option value="">Task Distribution by Deadline</option> -->
       </select>
       <select
         class="border-none focus:[box-shadow:none] bg-slate-200 rounded-md shadow font-medium text-slate-900"
+        v-model="timeFilter"
       >
-        <option value="">Last Hour</option>
-        <option value="">Today</option>
-        <option value="">Yesterday</option>
-        <option value="">Past Weak</option>
-        <option value="">Past Month</option>
-        <option value="">Past Year</option>
-        <option value="">All Time</option>
+        <option value="last_hour">Last Hour</option>
+        <option value="today">Today</option>
+        <option value="past_weak">Past Weak</option>
+        <option value="past_month">Past Month</option>
+        <option value="past_year">Past Year</option>
+        <option value="all">All Time</option>
       </select>
     </div>
-    <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
+
+    <div v-if="statLoading" class="w-full my-24 flex items-center justify-center">
+      <v-progress-circular :size="50" :width="5" color="purple" indeterminate></v-progress-circular>
+    </div>
+    <apexchart
+      v-else
+      class="-z-10 relative mt-5"
+      width="500"
+      type="donut"
+      :options="options"
+      :series="series"
+    ></apexchart>
   </div>
 </template>
 <script setup>
-import { computed } from 'vue'
+import axios from 'axios'
+import { computed, ref, watchEffect } from 'vue'
 
-// import VueApexCharts from 'vue-apexcharts'
 const options = computed(() => ({
-  chart: {
-    id: 'vuechart-example'
-  },
-  xaxis: {
-    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+  labels: labels.value,
+  theme: {
+    palette: 'palette7'
   }
 }))
 
-const series = [
-  {
-    name: 'series-1',
-    data: [30, 40, 45, 50, 49, 60, 70, 91]
-  }
-]
+const statLoading = ref(false)
+
+const series = ref([])
+const labels = ref([])
+
+const timeFilter = ref('all')
+const statistics = ref('completed_vs_pending_tasks')
+
+watchEffect(() => {
+  statLoading.value = true
+  axios
+    .get(`/api/user/analysis`, {
+      params: {
+        time_range: timeFilter.value,
+        statistics: statistics.value
+      }
+    })
+    .then((res) => {
+      console.log(res.data)
+      series.value = res.data.series
+      labels.value = res.data.labels
+    })
+    .finally(() => {
+      statLoading.value = false
+    })
+})
 </script>
