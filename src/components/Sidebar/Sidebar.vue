be5018bd29ca01b9d1e2866e5fd9661236696ea0
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-29 22:33:44 +0530

    added circular progress bar to task card in user dashboard

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -3,7 +3,7 @@
     <div
       v-if="!ismobile"
       :class="{ 'w-64': open, 'w-16': !open }"
-      class="bg-black text-black h-screen overflow-hidden transition-all duration-300 fixed z-10 shadow-md"
+      class="bg-black text-black h-screen overflow-hidden transition-all duration-300 fixed z-50 shadow-md"
     >
       <!-- fixed top-0 start-0 z-50 -->
       <div class="py-4 flex items-center justify-center">
---
+++ src/components/ui/CircularProgress.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center">
+    <v-progress-circular :model-value="value" :rotate="360" :size="60" :width="8" color="#16A34A">
+      <template v-slot:default>
+        <div class="text-slate-700 font font-semibold text-sm">{{ value }}%</div>
+      </template>
+    </v-progress-circular>
+  </div>
+</template>
+
+<script setup>
+defineProps(['value'])
+</script>
