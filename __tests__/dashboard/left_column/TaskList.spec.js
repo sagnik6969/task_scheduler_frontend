d5176f2cb1215e7bfdbba1334a2e6b8c861a9927
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-03-02 14:21:58 +0530

    added more tests

--- __tests__/dashboard/left_column/TaskList.spec.js
+++ __tests__/dashboard/left_column/TaskList.spec.js
@@ -7,6 +7,7 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 import * as matchers from '@testing-library/jest-dom/matchers'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
+import userEvent from '@testing-library/user-event'
 expect.extend(matchers)
 
 const tasks = [
@@ -49,6 +50,23 @@ beforeEach(() => {
   }
 })
 
+let clickedLink = null
+beforeEach(() => (clickedLink = null))
+const routerLink = {
+  template: `<div @click="handleCLick">
+  <slot>
+  </slot>
+  </div>`,
+  props: ['to'],
+  methods: {
+    handleCLick() {
+      clickedLink = this.to
+    }
+  }
+}
+
+const routerPushFn = vi.fn()
+
 const setup = async () => {
   render(TaskList, {
     global: {
@@ -57,10 +75,13 @@ const setup = async () => {
         $route: {
           query: {}
         }
+        // $router: {
+        //   push: routerPushFn
+        // }
       },
       stubs: {
         'v-icon': true,
-        'router-link': true,
+        'router-link': routerLink,
         'v-tooltip': true
       }
     }
@@ -70,8 +91,11 @@ const setup = async () => {
 describe('Task List', () => {
   it('has Tasks heading', async () => {
     await setup()
+    expect(screen.getByRole('heading', { name: 'Tasks' })).toBeInTheDocument()
+  })
 
-    expect(screen.getByText('Tasks')).toBeInTheDocument()
+  it('has search tasks input', async () => {
+    await setup(), expect(screen.getByPlaceholderText('Search tasks.....')).toBeInTheDocument()
   })
 
   describe('when task list is empty', () => {
@@ -79,6 +103,36 @@ describe('Task List', () => {
       store.getters.userTasks = []
       await setup()
       expect(screen.getByText(`looks like you haven't added any task yet!`)).toBeInTheDocument()
+    })
+  })
+
+  describe('when task list contains a task', () => {
+    it('displays the task', async () => {
+      await setup()
+      expect(screen.getByText('Task Title')).toBeInTheDocument()
+    })
+  })
+
+  describe('Task List Nav', () => {
+    describe.each([
+      { name: 'All Tasks', query: '' },
+      { name: 'Most Important', query: 'most_important' },
+      { name: 'Near Deadline', query: 'near_deadline' },
+      { name: 'Least Progress', query: 'least_progress' },
+      { name: 'Assigned By admin', query: 'assigned_by_admin' }
+    ])('when user clicks $name link', ({ name, query }) => {
+      it('it modifies the route query accordingly', async () => {
+        await setup()
+        const user = userEvent.setup()
+        const routerLink = screen.getByText(name)
+        expect(routerLink).toBeInTheDocument()
+        await user.click(routerLink)
+        expect(clickedLink).toStrictEqual({
+          query: {
+            filter: query
+          }
+        })
+      })
     })
   })
 })
