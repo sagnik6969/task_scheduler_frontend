abb65ca5753a212a0c50329549f1049a86fb70e5
Aishwary <Aishwary.009.shrivastava@culturtap.com>
2024-03-03 12:01:11 +0530

    working on profile

--- src/components/Profile/UserProfile.vue
+++ src/components/Profile/UserProfile.vue
@@ -3,41 +3,63 @@
     <user-notification-bar
       class="extracss top-0 right-0 left-0 py-3 w-full bg-white xl:static xl:mr-0 xl:mt-0 xl:pr-0"
     ></user-notification-bar>
-    <div class="sm:flex w-full p-5 md:p-20 gap-5 h-full">
-      <div class="shadow-md border-2 border-black bg-green-200 rounded-lg">
-        <div class="flex justify-center items-center mt-5">
+    <div class="sm:flex w-full p-5 md:pl-20 md:pr-6 gap-5 h-3/3">
+      <div
+        class="shadow-lg border-2 border-gray-300 bg-gradient-to-br from-green-200 to-green-100 rounded-lg p-5 h-auto md:h-2/3 transition duration-300 ease-in-out"
+      >
+        <div class="flex justify-center items-center mb-4">
           <img
-            class="object-cover rounded-full h-24 w-24 min-h-12 min-w-12"
-            src="https://avatars.githubusercontent.com/u/47273253?v=4"
+            class="object-cover rounded-full h-24 w-24 min-h-12 min-w-12 border-4 border-white hover:border-green-400"
+            :src="'https://avatars.githubusercontent.com/u/47273253?v=4'"
             alt="profile pic"
           />
         </div>
-        <div class="flex justify-center items-center mt-5">
-          <h1 class="text-3xl font-bold">{{ userDetails ? userDetails.name : name }}</h1>
-        </div>
-        <div class="p-5 md:p-14 flex flex-col justify-start">
+        <div class="flex flex-col justify-start items-center mb-4">
+          <h1 class="text-3xl font-bold text-gray-800">
+            {{ userDetails ? userDetails.name : name }}
+          </h1>
           <div
-            class="flex p-2 border-2 border-black rounded-sm items-center w-full justify-between mb-2"
+            class="rounded-full h-6 w-20 flex items-center justify-center mt-2 text-sm font-semibold text-white bg-green-500"
           >
-            <h1 class="text-lg sm:text-2xl font-semibold">My profile</h1>
-            <p class="text-sm sm:text-md">
-              Last created at:
-              <span class="text-xs sm:text-sm block">{{
-                userDetails ? formatDate(userDetails.created_at) : formatDate(date)
-              }}</span>
-            </p>
+            {{ userDetails.is_admin ? 'Admin' : 'User' }}
           </div>
-          <div class="flex w-full border-2 border-black items-center p-2 justify-between mb-2">
-            <h1 class="text-lg sm:text-2xl font-semibold">Email:</h1>
-            <p class="text-sm sm:text-md">{{ userDetails ? userDetails.email : Email }}</p>
+        </div>
+        <div class="pt-3 md:pl-14 pr-14 flex flex-col justify-start">
+          <h1 class="text-center mb-5 font-semibold text-2xl text-gray-800">Profile Details</h1>
+          <div class="flex flex-col space-y-4">
+            <div
+              class="p-4 rounded-lg bg-gray-100 hover:bg-gray-200 transition-colors duration-300"
+            >
+              <h1 class="text-lg font-semibold mb-1 text-gray-800">Email:</h1>
+              <p class="text-base text-gray-600">
+                {{ userDetails ? userDetails.email : 'Email' }}
+              </p>
+            </div>
+            <div
+              class="p-4 rounded-lg bg-gray-100 hover:bg-gray-200 transition-colors duration-300"
+            >
+              <h1 class="text-lg font-semibold mb-1 text-gray-800">Registered:</h1>
+              <p class="text-base text-gray-600">
+                <span>{{ userDetails ? formatDate(userDetails.created_at) : '' }}</span>
+              </p>
+            </div>
+            <div
+              class="p-4 rounded-lg bg-gray-100 hover:bg-gray-200 transition-colors duration-300"
+            >
+              <h1 class="text-lg font-semibold mb-1 text-gray-800">Updated At:</h1>
+              <p class="text-base text-gray-600">
+                {{ userDetails ? formatDate(userDetails.updated_at) : '' }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
+
       <div class="flex flex-col w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-2/3">
         <TaskCounter />
         <div class="flex">
           <Statistics class="xl:w-[500px]" />
-          <Efficiency />
+          <!-- <Efficiency /> -->
         </div>
       </div>
     </div>
@@ -52,34 +74,12 @@ import Efficiency from '@/components/dashboard/right_column/Efficiency.vue'
 import { useStore } from 'vuex'
 const store = useStore()
 const name = store.getters.userName
-const date = store.getters.User.created_at
-const Email = store.getters.User.email
-const isverified = store.getters.User.email_verified_at
+const userDetails = store.getters.User
+
 const formatDate = (dateString) => {
-  const currentDate = new Date()
   const date = new Date(dateString)
-  const timeDifference = currentDate.getTime() - date.getTime()
-
-  const seconds = Math.floor(timeDifference / 1000)
-  const minutes = Math.floor(seconds / 60)
-  const hours = Math.floor(minutes / 60)
-  const days = Math.floor(hours / 24)
-  const months = Math.floor(days / 30)
-  const years = Math.floor(days / 365)
-
-  if (years > 0) {
-    return `${years} year${years > 1 ? 's' : ''} ago`
-  } else if (months > 0) {
-    return `${months} month${months > 1 ? 's' : ''} ago`
-  } else if (days > 0) {
-    return `${days} day${days > 1 ? 's' : ''} ago`
-  } else if (hours > 0) {
-    return `${hours} hour${hours > 1 ? 's' : ''} ago`
-  } else if (minutes > 0) {
-    return `${minutes} minute${minutes > 1 ? 's' : ''} ago`
-  } else {
-    return `${seconds} second${seconds > 1 ? 's' : ''} ago`
-  }
+  const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  return formattedDate
 }
 </script>
 
